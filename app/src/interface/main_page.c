ce5ebd14324ca962cceb15aeecedb2cfdc9ff0f3
cempod <cempod@gmail.com>
2024-12-16 13:16:46 +0300

    Refactoring

--- app/src/interface/main_page.c
+++ app/src/interface/main_page.c
@@ -4,70 +4,82 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-
-void place_card(int x_size, int y_size, int x_offset, int y_offset, lv_obj_t ** parent, lv_obj_t ** card, lv_obj_t ** header, lv_obj_t ** label, lv_obj_t ** header_label);
-void place_headless_card(int x_size, int y_size, int x_offset, int y_offset, lv_obj_t ** parent, lv_obj_t ** card);
-void place_label(int x_offset, int y_offset, lv_obj_t ** parent, lv_obj_t ** label, const lv_font_t * font);
+void place_card(card_t * card, lv_obj_t * parent);
+void set_card_theme(card_t * card);
+void place_datetime_card(datetime_card_t * card, lv_obj_t * parent);
+void set_datetime_card_theme(datetime_card_t * card);
+void place_top_panel(top_panel_t * panel, lv_obj_t * parent);
+void set_top_panel_theme(top_panel_t * panel);
+lv_obj_t * place_label(int x_offset, int y_offset, lv_obj_t * parent, const lv_font_t * font);
 void set_label_text(lv_obj_t * label, const char* msg, ...);
-void set_label_color(lv_obj_t * label, lv_color_t color);
-void add_tile(lv_obj_t * parent);
 
 static lv_obj_t * main_screen;
-static lv_obj_t * temperature_card;
-static lv_obj_t * temperature_card_header;
-static lv_obj_t * pressure_card;
-static lv_obj_t * pressure_card_header;
-static lv_obj_t * humidity_card;
-static lv_obj_t * humidity_card_header;
-static lv_obj_t * co2_card;
-static lv_obj_t * co2_card_header;
-static lv_obj_t * time_card;
-static lv_obj_t * top_panel;
-static lv_obj_t * temperature_header_label;
-static lv_obj_t * pressure_header_label;
-static lv_obj_t * humidity_header_label;
-static lv_obj_t * co2_header_label;
-static lv_obj_t * time_label;
-static lv_obj_t * date_label;
-static lv_obj_t * week_label;
-static lv_obj_t * temperature_label;
-static lv_obj_t * pressure_label;
-static lv_obj_t * humidity_label;
-static lv_obj_t * co2_label;
-static lv_obj_t * status_label;
+
+static card_t temperature_card = {
+    .x_size = 200,
+    .y_size = 150,
+    .x_offset = 5,
+    .y_offset = 55,
+    .header_string = "Температура"
+};
+
+static card_t pressure_card = {
+    .x_size = 150,
+    .y_size = 100,
+    .x_offset = 5,
+    .y_offset = 215,
+    .header_string = "Давление"
+};
+
+static card_t humidity_card = {
+    .x_size = 150,
+    .y_size = 100,
+    .x_offset = 165,
+    .y_offset = 215,
+    .header_string = "Влажность"
+};
+
+static card_t co2_card = {
+    .x_size = 150,
+    .y_size = 100,
+    .x_offset = 325,
+    .y_offset = 215,
+    .header_string = "CO2"
+};
+
+static datetime_card_t datetime_card = {
+    .x_size = 260,
+    .y_size = 150,
+    .x_offset = 215,
+    .y_offset = 55
+};
+
+top_panel_t top_panel = {
+    .x_size = 470,
+    .y_size = 40,
+    .x_offset = 5,
+    .y_offset = 5,
+};
 
 void
 init_main_page(void) {
     main_screen = lv_obj_create(NULL);
-    lv_obj_set_style_bg_color(main_screen, get_colors().background_color, 0);
-    place_card(200, 150, 5, 55, &main_screen, &temperature_card, &temperature_card_header, &temperature_label, &temperature_header_label);
-    set_label_text(temperature_header_label, "Температура");
-    place_card(150, 100, 5, 215, &main_screen, &pressure_card, &pressure_card_header, &pressure_label, &pressure_header_label);
-    set_label_text(pressure_header_label, "Давление");
-    place_card(150, 100, 165, 215, &main_screen, &humidity_card, &humidity_card_header, &humidity_label, &humidity_header_label);
-    set_label_text(humidity_header_label, "Влажность");
-    place_card(150, 100, 325, 215, &main_screen, &co2_card, &co2_card_header, &co2_label, &co2_header_label);
-    set_label_text(co2_header_label, "CO2");
-    place_headless_card(260, 150, 215, 55, &main_screen, &time_card);
-    place_headless_card(470, 40, 5, 5, &main_screen, &top_panel);
-    add_tile(top_panel);
-    lv_obj_set_style_bg_color(top_panel, get_colors().header_color, 0);
-    place_label(0,0, &time_card, &time_label, get_colors().big_font);
-    place_label(0,-50, &time_card, &date_label, get_colors().main_font);
-    place_label(0,50, &time_card, &week_label, get_colors().main_font);
-    set_label_color(time_label, get_colors().main_font_color);
-    set_label_color(date_label, get_colors().main_font_color);
-    set_label_color(week_label, get_colors().main_font_color);
-    set_label_text(time_label, "22:44");
-    set_label_text(date_label, "Декабрь 12");
-    set_label_text(week_label, "Четверг");
-    lv_obj_set_style_text_font(temperature_label, get_colors().big_font, 0);
-    lv_obj_align(temperature_label, LV_ALIGN_CENTER, 0, 18);
-    set_label_text(temperature_label, "27.3°C");
-    set_label_text(pressure_label, "765mm");
-    set_label_text(humidity_label, "49.8%%");
-    set_label_text(co2_label, "632ppm");
-    set_label_text(status_label, "Показатели в норме");
+    place_card(&temperature_card, main_screen);
+    lv_obj_set_style_text_font(temperature_card.label,get_colors().big_font, 0);
+    lv_obj_align(temperature_card.label, LV_ALIGN_CENTER, 0, 18);
+    set_label_text(temperature_card.label, "27.3°C");
+    place_card(&pressure_card, main_screen);
+    set_label_text(pressure_card.label, "760mm");
+    place_card(&humidity_card, main_screen);
+    set_label_text(humidity_card.label, "45%%");
+    place_card(&co2_card, main_screen);
+    set_label_text(co2_card.label, "1200ppm");
+    place_datetime_card(&datetime_card, main_screen);
+    set_label_text(datetime_card.date_top_label, "16 декабря");
+    set_label_text(datetime_card.date_bottom_label, "Понедельник");
+    set_label_text(datetime_card.time_label, "12:44");
+    place_top_panel(&top_panel, main_screen);
+    set_label_text(top_panel.status_label, "Показатели в норме");
 }
 
 void
@@ -77,46 +89,99 @@ load_main_page(void) {
 }
 
 void 
-place_card(int x_size, int y_size, int x_offset, int y_offset, lv_obj_t ** parent, lv_obj_t ** card, lv_obj_t ** header, lv_obj_t ** label, lv_obj_t ** header_label) {
-    *card = lv_obj_create(*parent);
-    lv_obj_set_style_bg_color(*card, get_colors().card_background_color, 0);
-    lv_obj_align(*card, LV_ALIGN_TOP_LEFT, x_offset, y_offset);
-    lv_obj_set_size(*card, x_size, y_size);
-    lv_obj_set_style_pad_all(*card,0,0);
-    lv_obj_set_style_border_color(*card, get_colors().border_color, 0);
-    *header = lv_obj_create(*card);
-    lv_obj_set_style_bg_color(*header, get_colors().header_color, 0);
-    lv_obj_set_size(*header, x_size-4, 35);
-    lv_obj_align(*header, LV_ALIGN_TOP_MID, 0, 0);
-    lv_obj_set_style_border_side(*header, LV_BORDER_SIDE_NONE, 0);
-    lv_obj_set_style_radius(*header, 0, 0);
-    lv_obj_set_style_pad_all(*header,0,0);
-    lv_obj_set_style_bg_color (*header , get_colors().header_color, 0);
-    place_label(0,0, header, header_label, get_colors().main_font);
-    place_label(0,21, card, label, get_colors().medium_font);
-    set_label_color(*label, get_colors().main_font_color);
+place_card(card_t * card, lv_obj_t * parent) {
+    card->parent = parent;
+    card->card = lv_obj_create(card->parent);
+    lv_obj_align(card->card, LV_ALIGN_TOP_LEFT, card->x_offset, card->y_offset);
+    lv_obj_set_size(card->card, card->x_size, card->y_size);
+    lv_obj_set_style_pad_all(card->card,0,0);
+    card->header = lv_obj_create(card->card);
+    lv_obj_set_size(card->header, card->x_size-4, 35);
+    lv_obj_align(card->header, LV_ALIGN_TOP_MID, 0, 0);
+    lv_obj_set_style_border_side(card->header, LV_BORDER_SIDE_NONE, 0);
+    lv_obj_set_style_radius(card->header, 0, 0);
+    lv_obj_set_style_pad_all(card->header,0,0);
+    card->header_label = place_label(0,0, card->header, get_colors().main_font);
+    card->label = place_label(0,21, card->card, get_colors().medium_font);
+    set_label_text(card->header_label, card->header_string);
+    set_card_theme(card);
+}
+
+void
+set_card_theme(card_t * card) {
+    lv_obj_set_style_bg_color(card->card, get_colors().card_background_color, 0);
+    lv_obj_set_style_border_color(card->card, get_colors().border_color, 0);
+    lv_obj_set_style_bg_color(card->header, get_colors().header_color, 0);
+    lv_obj_set_style_bg_color (card->header , get_colors().header_color, 0);
+    lv_obj_set_style_text_color(card->label, get_colors().main_font_color, 0);
+    lv_obj_set_style_text_color(card->header_label, get_colors().header_font_color, 0);
 }
 
 void 
-place_headless_card(int x_size, int y_size, int x_offset, int y_offset, lv_obj_t ** parent, lv_obj_t ** card) {
-    *card = lv_obj_create(*parent);
-    lv_obj_set_style_bg_color(*card, get_colors().card_background_color, 0);
-    lv_obj_set_style_border_color(*card, get_colors().border_color, 0);
-    lv_obj_align(*card, LV_ALIGN_TOP_LEFT, x_offset, y_offset);
-    lv_obj_set_size(*card, x_size, y_size);
-    lv_obj_set_style_pad_all(*card,0,0);
+place_datetime_card(datetime_card_t * card, lv_obj_t * parent) {
+    card->card = lv_obj_create(parent);
+    lv_obj_align(card->card, LV_ALIGN_TOP_LEFT, card->x_offset, card->y_offset);
+    lv_obj_set_size(card->card, card->x_size, card->y_size);
+    lv_obj_set_style_pad_all(card->card,0,0);
+    card->time_label = place_label(0,0, card->card, get_colors().big_font);
+    card->date_top_label = place_label(0,-50, card->card, get_colors().main_font);
+    card->date_bottom_label = place_label(0,50, card->card, get_colors().main_font);
+    set_datetime_card_theme(card);
+}
+
+void
+set_datetime_card_theme(datetime_card_t * card) {
+    lv_obj_set_style_bg_color(card->card, get_colors().card_background_color, 0);
+    lv_obj_set_style_border_color(card->card, get_colors().border_color, 0);
+    lv_obj_set_style_text_color(card->date_top_label, get_colors().main_font_color, 0);
+    lv_obj_set_style_text_color(card->date_bottom_label, get_colors().main_font_color, 0);
+    lv_obj_set_style_text_color(card->time_label, get_colors().main_font_color, 0);
 }
 
 void 
-place_label(int x_offset, int y_offset, lv_obj_t ** parent, lv_obj_t ** label, const lv_font_t * font) {
-    *label = lv_label_create(*parent);
-    lv_label_set_long_mode(*label, LV_LABEL_LONG_WRAP);
-    set_label_text(*label, "Label");
-    lv_obj_set_width(*label, LV_SIZE_CONTENT); 
-    lv_obj_set_style_text_align(*label, LV_TEXT_ALIGN_CENTER, 0);
-    lv_obj_align(*label, LV_ALIGN_CENTER, x_offset, y_offset);
-    lv_obj_set_style_text_font(*label,font,0);
-    lv_obj_set_style_text_color(*label, get_colors().header_font_color, 0);
+place_top_panel(top_panel_t * panel, lv_obj_t * parent) {
+    panel->card = lv_obj_create(parent);
+    lv_obj_align(panel->card, LV_ALIGN_TOP_LEFT, panel->x_offset, panel->y_offset);
+    lv_obj_set_size(panel->card, panel->x_size, panel->y_size);
+    lv_obj_set_style_pad_all(panel->card,0,0);
+    panel->tile_view = lv_tileview_create(panel->card);
+    lv_obj_set_scrollbar_mode(panel->tile_view, LV_SCROLLBAR_MODE_OFF);
+    panel->tile1 = lv_tileview_add_tile(panel->tile_view, 0, 0, LV_DIR_RIGHT);
+    panel->status_label = place_label(0,0, panel->tile1, get_colors().main_font);
+    lv_obj_set_width(panel->status_label, 466); 
+    panel->tile2 = lv_tileview_add_tile(panel->tile_view, 1, 0, LV_DIR_LEFT);
+    panel->settings_button = lv_btn_create(panel->tile2);
+    lv_obj_clear_state(panel->settings_button, LV_STATE_FOCUS_KEY);
+    lv_obj_set_style_shadow_opa(panel->settings_button, LV_OPA_TRANSP, 0);
+    lv_obj_set_style_border_width(panel->settings_button, 1, 0);
+    lv_obj_set_style_pad_all(panel->settings_button, 3, 0);
+    lv_obj_align(panel->settings_button, LV_ALIGN_RIGHT_MID, -3, 0);
+    panel->settings_button_label = place_label(0,0, panel->settings_button, get_colors().main_font);
+    set_label_text(panel->settings_button_label, "Настройки");
+    set_top_panel_theme(panel);
+}
+
+void
+set_top_panel_theme(top_panel_t * panel) {
+    lv_obj_set_style_bg_color(panel->card, get_colors().card_background_color, 0);
+    lv_obj_set_style_border_color(panel->card, get_colors().border_color, 0);
+    lv_obj_set_style_bg_color(panel->tile_view, get_colors().header_color, 0);
+    lv_obj_set_style_bg_color(panel->settings_button, get_colors().header_color, 0);
+    lv_obj_set_style_border_color(panel->settings_button, get_colors().header_font_color, 0);
+    lv_obj_set_style_text_color(panel->status_label, get_colors().header_font_color, 0);
+    lv_obj_set_style_text_color(panel->settings_button_label, get_colors().header_font_color, 0);
+}
+
+lv_obj_t * 
+place_label(int x_offset, int y_offset, lv_obj_t * parent, const lv_font_t * font) {
+    lv_obj_t * label = lv_label_create(parent);
+    lv_label_set_long_mode(label, LV_LABEL_LONG_WRAP);
+    set_label_text(label, "Label");
+    lv_obj_set_width(label, LV_SIZE_CONTENT); 
+    lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, 0);
+    lv_obj_align(label, LV_ALIGN_CENTER, x_offset, y_offset);
+    lv_obj_set_style_text_font(label,font,0);
+    return label;
 }
 
 void
@@ -130,60 +195,13 @@ set_label_text(lv_obj_t * label, const char* msg, ...) {
 }
 
 void
-set_label_color(lv_obj_t * label, lv_color_t color) {
-    lv_obj_set_style_text_color(label, color, 0);
-}
-
-void
 set_theme(int theme) {
     set_colors(theme);
     lv_obj_set_style_bg_color(main_screen, get_colors().background_color, 0);
-    lv_obj_set_style_bg_color(temperature_card, get_colors().card_background_color, 0);
-    lv_obj_set_style_bg_color(pressure_card, get_colors().card_background_color, 0);
-    lv_obj_set_style_bg_color(humidity_card, get_colors().card_background_color, 0);
-    lv_obj_set_style_bg_color(co2_card, get_colors().card_background_color, 0);
-    lv_obj_set_style_bg_color(time_card, get_colors().card_background_color, 0);
-    lv_obj_set_style_bg_color(temperature_card_header, get_colors().header_color, 0);
-    lv_obj_set_style_bg_color(pressure_card_header, get_colors().header_color, 0);
-    lv_obj_set_style_bg_color(humidity_card_header, get_colors().header_color, 0);
-    lv_obj_set_style_bg_color(co2_card_header, get_colors().header_color, 0);
-    lv_obj_set_style_bg_color(top_panel, get_colors().header_color, 0);
-    set_label_color(temperature_header_label, get_colors().header_font_color);
-    set_label_color(pressure_header_label, get_colors().header_font_color);
-    set_label_color(humidity_header_label, get_colors().header_font_color);
-    set_label_color(time_label, get_colors().main_font_color);
-    set_label_color(date_label, get_colors().main_font_color);
-    set_label_color(week_label, get_colors().main_font_color);
-    set_label_color(temperature_label, get_colors().main_font_color);
-    set_label_color(pressure_label, get_colors().main_font_color);
-    set_label_color(humidity_label, get_colors().main_font_color);
-    set_label_color(co2_label, get_colors().main_font_color);
-    lv_obj_set_style_border_color(temperature_card, get_colors().border_color, 0);
-    lv_obj_set_style_border_color(pressure_card, get_colors().border_color, 0);
-    lv_obj_set_style_border_color(humidity_card, get_colors().border_color, 0);
-    lv_obj_set_style_border_color(co2_card, get_colors().border_color, 0);
-    lv_obj_set_style_border_color(time_card, get_colors().border_color, 0);
-    lv_obj_set_style_border_color(top_panel, get_colors().border_color, 0);
-}
-
-void 
-add_tile(lv_obj_t * parent) {
-    lv_obj_t * tile_view = lv_tileview_create(parent);
-    lv_obj_set_style_bg_color(tile_view, get_colors().header_color, 0);
-    lv_obj_set_scrollbar_mode(tile_view, LV_SCROLLBAR_MODE_OFF);
-    lv_obj_t * tile1 = lv_tileview_add_tile(tile_view, 0, 0, LV_DIR_RIGHT);
-    place_label(0,0, &tile1, &status_label, get_colors().main_font);
-    lv_obj_set_width(status_label, 466); 
-    lv_obj_t * tile2 = lv_tileview_add_tile(tile_view, 1, 0, LV_DIR_LEFT);
-    lv_obj_t * settings_button = lv_btn_create(tile2);
-    lv_obj_clear_state(settings_button, LV_STATE_FOCUS_KEY);
-    lv_obj_set_style_shadow_opa(settings_button, LV_OPA_TRANSP, 0);
-    lv_obj_set_style_bg_color(settings_button, get_colors().header_color, 0);
-    lv_obj_set_style_border_color(settings_button, get_colors().header_font_color, 0);
-    lv_obj_set_style_border_width(settings_button, 1, 0);
-    lv_obj_set_style_pad_all(settings_button, 3, 0);
-    lv_obj_align(settings_button, LV_ALIGN_RIGHT_MID, -3, 0);
-    lv_obj_t * settings_button_label;
-    place_label(0,0, &settings_button, &settings_button_label, get_colors().main_font);
-    set_label_text(settings_button_label, "Настройки");
+    set_card_theme(&temperature_card);
+    set_card_theme(&pressure_card);
+    set_card_theme(&humidity_card);
+    set_card_theme(&co2_card);
+    set_datetime_card_theme(&datetime_card);
+    set_top_panel_theme(&top_panel);
 }
--- app/src/interface/main_page.h
+++ app/src/interface/main_page.h
@@ -7,6 +7,46 @@
 #include "lvgl/lvgl.h"
 #endif
 
+typedef struct {
+    int x_size;
+    int y_size;
+    int x_offset;
+    int y_offset;
+    const char* header_string;
+    lv_obj_t * parent;
+    lv_obj_t * card;
+    lv_obj_t * header;
+    lv_obj_t * label;
+    lv_obj_t * header_label;
+} card_t;
+
+typedef struct {
+    int x_size;
+    int y_size;
+    int x_offset;
+    int y_offset;
+    lv_obj_t * parent;
+    lv_obj_t * card;
+    lv_obj_t * date_top_label;
+    lv_obj_t * date_bottom_label;
+    lv_obj_t * time_label;
+} datetime_card_t;
+
+typedef struct {
+    int x_size;
+    int y_size;
+    int x_offset;
+    int y_offset;
+    lv_obj_t * parent;
+    lv_obj_t * card;
+    lv_obj_t * tile_view;
+    lv_obj_t * tile1;
+    lv_obj_t * tile2;
+    lv_obj_t * settings_button;
+    lv_obj_t * status_label;
+    lv_obj_t * settings_button_label;
+} top_panel_t;
+
 void init_main_page(void);
 void load_main_page(void);
 void set_theme(int theme);
